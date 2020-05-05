e0310620843ba2c300fe665e4d336af63540e5ca
OxenCotin <ypei@mit.edu>
2020-05-05 14:54:53 -0400

    fix up lstm.py code

--- lstm.py
+++ lstm.py
@@ -1,8 +1,9 @@
 import numpy as np
 import utils
+import keras
 from keras.preprocessing.text import Tokenizer
 from keras import Sequential
-from keras.layers import Embedding, LSTM, Dense, Dropout, GlobalMaxPool1D
+from keras.layers import Embedding, LSTM, Dense, Dropout, GlobalMaxPool1D, SpatialDropout1D
 from keras.preprocessing.sequence import pad_sequences
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint
@@ -18,7 +19,7 @@ MAX_TEXT_LEN = 250
 OOV_TOKEN = "<OOV>"
 NUM_GENRES = 19
 
-INIT_LR = .005
+INIT_LR = .075
 NUM_EPOCHS = 10
 
 # TODO replace this later one
@@ -75,38 +76,63 @@ def create_baseline():
 
     optimizer = Adam(lr=INIT_LR, decay=INIT_LR / NUM_EPOCHS)
 
-    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['categorical_accuracy'])
 
     return model
 
 def create_lstm():
     # TODO
     model = Sequential()
-    model.add(Embedding(VOCAB_SIZE, 20, input_length=MAX_TEXT_LEN))
-    model.add(Dropout(rate=.15))
+    model.add(Embedding(VOCAB_SIZE, EMBEDDING_DIM, input_length=MAX_TEXT_LEN))
+    model.add(SpatialDropout1D(.2))
     model.add(LSTM(100, dropout=.2, recurrent_dropout=.2))
+    # model.add(GlobalMaxPool1D())
     model.add(Dense(NUM_GENRES, activation='sigmoid'))
 
     optimizer = Adam(lr=INIT_LR, decay=INIT_LR / NUM_EPOCHS)
 
-    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['categorical_accuracy'])
 
     return model
 
-print("Creating Model")
+# print("Creating Model")
+#
+# baseline_model = create_baseline()
+# print("Model created")
+#
+# callbacks = [
+#     EarlyStopping(patience=4),
+#     ModelCheckpoint(filepath='baseline-nn.h5', save_best_only=True)
+# ]
+#
+# history = baseline_model.fit(x_train, y_train,
+#                              epochs=NUM_EPOCHS,
+#                              batch_size=1,
+#                              validation_split=.1,
+#                              callbacks=callbacks
+#
+# )
 
-baseline_model = create_baseline()
-print("Model created")
+# lstm = create_lstm()
+# print("Model created")
+#
+# callbacks = [
+#     EarlyStopping(patience=4),
+#     ModelCheckpoint(filepath='lstm-nn.h5', save_best_only=True)
+# ]
+#
+# from tensorflow.python.client import device_lib
+# print(device_lib.list_local_devices())
+#
+# history = lstm.fit(x_train, y_train,
+#                              epochs=NUM_EPOCHS,
+#                              batch_size=1,
+#                              validation_split=.1,
+#                              callbacks=callbacks
+#
+# )
 
-callbacks = [
-    EarlyStopping(patience=4),
-    ModelCheckpoint(filepath='baseline-nn.h5', save_best_only=True)
-]
-
-history = baseline_model.fit(x_train, y_train,
-                             epochs=NUM_EPOCHS,
-                             batch_size=1,
-                             validation_split=.1,
-                             callbacks=callbacks
-
-)
+simple_model = keras.models.load_model('lstm-nn.h5')
+metrics = simple_model.evaluate(x_test, y_test)
+print("{}: {}".format(simple_model.metrics_names[0], metrics[0]))
+print("{}: {}".format(simple_model.metrics_names[1], metrics[1]))
